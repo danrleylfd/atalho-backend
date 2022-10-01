a09d3713c099dca3b0771a39a679e07c8fbc2acd
danrleylfd <danrleylfd@gmail.com>
2022-10-01 17:01:21 -0300

    update src/app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-app.use(cors({ origin: "https://atalho.vercel.app" }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
